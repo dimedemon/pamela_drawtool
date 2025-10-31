79db9d7eca6644a0839115b1588048b12b525492
dimedemon <77274633+dimedemon@users.noreply.github.com>
2025-10-31 15:18:18 +0300

    Update version_info.py

--- desktop_app/dialogs/version_info.py
+++ desktop_app/dialogs/version_info.py
@@ -3,7 +3,7 @@
 
 Создает диалоговое окно, показывающее таблицу версий и биннингов.
 """
-
+import re
 import numpy as np
 from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QTableWidget, QTableWidgetItem,
                              QPushButton, QHBoxLayout, QWidget, QAbstractItemView,

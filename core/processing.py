2f0148c99a958021a2b78e392181247d48234a69
dimedemon <77274633+dimedemon@users.noreply.github.com>
2025-11-25 15:00:16 +0300

    Update processing.py

--- core/processing.py
+++ core/processing.py
@@ -1,5 +1,8 @@
 """
-Модуль Обработки (Фаза 3 - Radial Distribution)
+Модуль Обработки (Фаза 3 - ПОЛНЫЙ)
+Поддерживает:
+1. Energy/Rigidity Spectra (PlotKind 1, 2)
+2. Radial Distribution (PlotKind 4)
 """
 
 import os
@@ -19,12 +22,14 @@ def _load_mat_file(file_path):
         return None
 
 def _build_data_path(app_state: state.ApplicationState):
+    # Определяем 'er' (E или R биннинг)
     if app_state.ror_e == 2:
         er = f"R{app_state.eb}"
     else:
         er = f"E{app_state.eb}"
 
     version_float = float(app_state.flux_version.replace('v', ''))
+
     if version_float < 5.0:
         binningpath = f"stdbinning{app_state.pitchb}{app_state.lb}e{app_state.eb}"
     else:
@@ -47,20 +52,8 @@ def _find_bin_indices(edges, values):
     indices[indices >= len(edges) - 1] = len(edges) - 2
     return indices
 
-# --- СУЩЕСТВУЮЩАЯ ФУНКЦИЯ (SPECTRA) ---
+# --- ФУНКЦИЯ 1: СПЕКТРЫ (SPECTRA) ---
 def _get_spectra_data(app_state: state.ApplicationState, ax_index: int):
-    # ... (Код _get_spectra_data остается без изменений, как в прошлом шаге) ...
-    # (Для краткости я не дублирую его здесь, оставьте старый код этой функции)
-    # ...
-    # Если нужно, я могу продублировать его, но лучше просто добавить новую функцию ниже.
-    pass 
-
-# --- НОВАЯ ФУНКЦИЯ (RADIAL) ---
-def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
-    """
-    Порт DrawRadial.m
-    Строит зависимость потока от L-оболочки.
-    """
     plot_data_list = []
     base_path = _build_data_path(app_state)
     
@@ -68,11 +61,84 @@ def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
     L_edges = config.BIN_INFO['Lbin'][app_state.lb - 1]
     pitch_edges = config.BIN_INFO['pitchbin'][app_state.pitchb - 1]
     
-    # Вычисляем центры L-бинов (Ось X для этого графика)
+    # Ось X - это Энергия или Жесткость
+    if app_state.ror_e == 1: # E
+        E_edges = config.BIN_INFO['Ebin'][app_state.eb - 1]
+        E_centers = (E_edges[:-1] + E_edges[1:]) / 2
+        dE = (E_edges[1:] - E_edges[:-1]) / 2
+        x_data, dx_data = E_centers, dE
+        x_label = "E, GeV"
+    else: # R
+        R_edges = config.BIN_INFO['Rig'][app_state.eb - 1]
+        R_centers = (R_edges[:-1] + R_edges[1:]) / 2
+        dR = (R_edges[1:] - R_edges[:-1]) / 2
+        x_data, dx_data = R_centers, dR
+        x_label = "R, GV"
+
+    # Фиксированные параметры: L и Pitch
+    L_indices = _find_bin_indices(L_edges, app_state.l)
+    pitch_indices = _find_bin_indices(pitch_edges, app_state.pitch)
+    
+    periods = app_state.pam_pers 
+    if app_state.tbin == 'Separate Periods':
+        periods = [app_state.period]
+    elif not periods:
+        periods = [200]
+
+    for period in periods:
+        infile = os.path.join(base_path, f"RBflux_{period}.mat")
+        data = _load_mat_file(infile)
+        if data is None: continue
+            
+        if app_state.fullday:
+            try:
+                JJ = data['Jday']
+                dJJ = data['dJday']
+                NN = data['numevday']
+            except KeyError: return []
+            
+            for l_idx in np.unique(L_indices):
+                for p_idx in np.unique(pitch_indices):
+                    # J(L, E, P) -> берем срез [l, :, p] (все E)
+                    y_data = JJ[l_idx, :, p_idx]
+                    y_err = dJJ[l_idx, :, p_idx]
+                    n_events = NN[l_idx, :, p_idx]
+                    
+                    valid_indices = (n_events > app_state.n_min)
+                    if not np.any(valid_indices): continue
+                        
+                    label = (f"L=[{L_edges[l_idx]:.2f}-{L_edges[l_idx+1]:.2f}], "
+                             f"P=[{pitch_edges[p_idx]:.0f}-{pitch_edges[p_idx+1]:.0f}] deg, "
+                             f"Day={period}")
+                    
+                    plot_data_list.append({
+                        "ax_index": ax_index,
+                        "plot_type": "errorbar",
+                        "x": x_data[valid_indices],
+                        "y": y_data[valid_indices],
+                        "x_err": dx_data[valid_indices],
+                        "y_err": y_err[valid_indices],
+                        "label": label,
+                        "xlabel": x_label,
+                        "ylabel": "J",
+                        "xscale": "log",
+                        "yscale": "log"
+                    })
+    return plot_data_list
+
+# --- ФУНКЦИЯ 2: РАДИАЛЬНОЕ РАСПРЕДЕЛЕНИЕ (RADIAL) ---
+def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
+    plot_data_list = []
+    base_path = _build_data_path(app_state)
+    
+    L_edges = config.BIN_INFO['Lbin'][app_state.lb - 1]
+    pitch_edges = config.BIN_INFO['pitchbin'][app_state.pitchb - 1]
+    
+    # Ось X - это L
     L_centers = (L_edges[:-1] + L_edges[1:]) / 2
     dL = (L_edges[1:] - L_edges[:-1]) / 2
     
-    # Находим индексы для ФИКСИРОВАННЫХ параметров (Pitch и E/R)
+    # Фиксированные параметры: Pitch и E
     pitch_indices = _find_bin_indices(pitch_edges, app_state.pitch)
     
     if app_state.ror_e == 1: # E
@@ -88,7 +154,7 @@ def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
     if app_state.tbin == 'Separate Periods':
         periods = [app_state.period]
     elif not periods:
-        periods = [200] # Хардкод для теста
+        periods = [200]
 
     for period in periods:
         infile = os.path.join(base_path, f"RBflux_{period}.mat")
@@ -102,13 +168,9 @@ def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
                 NN = data['numevday']
             except KeyError: return []
             
-            # --- Цикл по E-бинам (фиксированным) ---
             for e_idx in np.unique(E_indices):
-                # --- Цикл по Pitch-бинам (фиксированным) ---
                 for p_idx in np.unique(pitch_indices):
-                    
-                    # Извлечение данных: J(L, E, P)
-                    # Нам нужны ВСЕ L (slice :), конкретный E, конкретный P
+                    # J(L, E, P) -> берем срез [:, e, p] (все L)
                     y_data = JJ[:, e_idx, p_idx]
                     y_err = dJJ[:, e_idx, p_idx]
                     n_events = NN[:, e_idx, p_idx]
@@ -116,7 +178,6 @@ def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
                     valid_indices = (n_events > app_state.n_min)
                     if not np.any(valid_indices): continue
                         
-                    # Лейбл
                     val_name = "E" if app_state.ror_e == 1 else "R"
                     val_unit = "GeV" if app_state.ror_e == 1 else "GV"
                     val_center = (E_edges[e_idx] + E_edges[e_idx+1])/2
@@ -128,14 +189,14 @@ def _get_radial_data(app_state: state.ApplicationState, ax_index: int):
                     plot_data_list.append({
                         "ax_index": ax_index,
                         "plot_type": "errorbar",
-                        "x": L_centers[valid_indices], # L по оси X
+                        "x": L_centers[valid_indices],
                         "y": y_data[valid_indices],
                         "x_err": dL[valid_indices],
                         "y_err": y_err[valid_indices],
                         "label": label,
                         "xlabel": "L-shell",
                         "ylabel": "J",
-                        "xscale": "linear", # L обычно в линейном масштабе
+                        "xscale": "linear",
                         "yscale": "log"
                     })
     return plot_data_list
@@ -147,12 +208,11 @@ def get_plot_data(app_state: state.ApplicationState, ax_index: int = 0):
 
     if gen == 1: # PAMELA exp. data
         if plot_kind == 1 or plot_kind == 2: # Spectra
-            # Вызываем старую функцию (убедитесь, что она есть в файле!)
-            return _get_spectra_data(app_state, ax_index) 
+            return _get_spectra_data(app_state, ax_index)
         
         elif plot_kind == 3: # pitch-angular
-            pass
+            pass # TODO
         elif plot_kind == 4: # Radial distribution
-            return _get_radial_data(app_state, ax_index) # <--- НОВЫЙ ВЫЗОВ
+            return _get_radial_data(app_state, ax_index)
             
     return []
